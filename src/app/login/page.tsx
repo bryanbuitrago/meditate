3fdcde681c51bde60d7a420b2048999d301b0013
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-27 14:50:46 -0400

    Add login route.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import LoginForm from '../components/Form';
+function LoginPage() {
+    return (
+        <div>
+            <h1>This is the login page</h1>
+            <LoginForm />
+        </div>
+    );
+}
+
+export default LoginPage;
