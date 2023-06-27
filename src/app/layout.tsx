ad37ff8159810ca7b0f22ae10e3236f5c6aa2c38
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-27 14:53:42 -0400

    Add providers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from './providers'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,11 +12,15 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
