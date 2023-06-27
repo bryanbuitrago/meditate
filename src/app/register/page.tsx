f51e62338608ef06a40cfe9c5885a305a671f5b9
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-27 14:16:12 -0400

    Add register page.

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+function RegisterPage() {
+    return (
+        <div>
+            <h1>
+                This is the register Page
+            </h1>
+        </div>
+    );
+}
+
+export default RegisterPage;
