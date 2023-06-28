9eff18e541102f1796cc51d6fac159118adfcdd4
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-28 17:41:01 -0400

    Modify to return both css and session providers.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,30 @@
-// app/providers.tsx
 'use client'
 
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { CacheProvider } from '@chakra-ui/next-js';
 
-export function Providers({ 
-    children 
-  }: { 
-  children: React.ReactNode 
-  }) {
+interface Props {
+  children?: React.ReactNode;
+}
+
+export function Providers({ children }: Props) {
   return (
     <CacheProvider>
       <ChakraProvider>
         {children}
       </ChakraProvider>
     </CacheProvider>
-  )
+  );
+}
+
+export default function NextAuthSessionProvider({ children }: Props) {
+  return (
+    <ChakraProvider>
+      <CacheProvider>
+        <SessionProvider>{children}</SessionProvider>
+      </CacheProvider>
+    </ChakraProvider>
+  );
 }
