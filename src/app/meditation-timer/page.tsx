896f3d8d9aa4f5afb359ca944eb4f1e4a50dfd41
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-28 17:31:07 -0400

    Import Timer component.

--- src/app/meditation-timer/page.tsx
+++ src/app/meditation-timer/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
+import Timer from '../components/Timer';
 function TimerPage() {
     return (
         <div>
             <h1>Meditation Timer Page</h1>
+            <Timer />
         </div>
     );
 }
