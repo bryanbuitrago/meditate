6f325261fc86c5c891bbae874818bbd354790107
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-28 17:36:59 -0400

    Add react-hook-form & wire signIn function to send data to api endpoint.

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,19 +1,42 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { signIn, SignInResponse } from 'next-auth/react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import GithubIcon from '../../../public/github-icon.svg'
 import GoogleIcon from '../../../public/google-icon.svg'
 import Image from 'next/image';
 import { Box, Button, FormControl, FormLabel, Input, Stack, Center, Text, Divider } from '@chakra-ui/react';
 
-const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    // Perform login logic here with username and password
-    console.log('Login form submitted');
-    console.log('Username:', username);
-    console.log('Password:', password);
+const LoginForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>();
+
+  const onSubmit = async (data: LoginFormValues) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: data.email,
+        password: data.password,
+      }) as SignInResponse;
+
+      if (result.error) {
+        console.error('Login failed:', result.error);
+        // Handle login failure
+      } else {
+        console.log('Login successful');
+        // Handle successful login
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      // Handle login error
+    }
   };
 
   const handleGoogleLogin = () => {
@@ -26,26 +49,18 @@ const LoginForm: React.FC = () => {
     console.log('GitHub login');
   };
 
-  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
   return (
     <Box maxWidth="md" mx="auto" mt={8} p={4}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={4}>
-          <FormControl id="username">
-            <FormLabel>Username</FormLabel>
+          <FormControl id="email">
+            <FormLabel>Email</FormLabel>
             <Input
-              type="text"
-              placeholder="Enter your username"
-              value={username}
-              onChange={handleUsernameChange}
+              type="email"
+              placeholder="Enter your email"
+              {...register('email', { required: true })}
             />
+            {errors.email && <span>Email is required</span>}
           </FormControl>
 
           <FormControl id="password">
@@ -53,9 +68,9 @@ const LoginForm: React.FC = () => {
             <Input
               type="password"
               placeholder="Enter your password"
-              value={password}
-              onChange={handlePasswordChange}
+              {...register('password', { required: true })}
             />
+            {errors.password && <span>Password is required</span>}
           </FormControl>
 
           <Button type="submit" colorScheme="blue">
@@ -92,3 +107,119 @@ const LoginForm: React.FC = () => {
 };
 
 export default LoginForm;
+
+
+// import { signIn, SignInResponse } from 'next-auth/react';
+// import React, { useState, ChangeEvent, FormEvent } from 'react';
+// import GithubIcon from '../../../public/github-icon.svg'
+// import GoogleIcon from '../../../public/google-icon.svg'
+// import Image from 'next/image';
+// import { Box, Button, FormControl, FormLabel, Input, Stack, Center, Text, Divider } from '@chakra-ui/react';
+
+// const LoginForm: React.FC = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = async (e: FormEvent) => {
+//     e.preventDefault();
+//     // Perform login logic here with email and password
+//     console.log('Login form submitted');
+//     console.log('Email:', email);
+//     console.log('Password:', password);
+
+//     try {
+//       const result = await signIn('credentials', {
+//         email,
+//         password,
+//         redirect: false, // Set redirect to false to handle the response manually
+//       }) as SignInResponse;
+  
+//       if (result.error) {
+//         console.error('Login failed:', result.error);
+//         // Handle login failure
+//       } else {
+//         console.log('Login successful');
+//         // Handle successful login
+//       }
+//     } catch (error) {
+//       console.error('Error during login:', error);
+//       // Handle login error
+//     }
+//   };
+
+//   const handleGoogleLogin = () => {
+//     // Handle Google login
+//     console.log('Google login');
+//   };
+
+//   const handleGitHubLogin = () => {
+//     // Handle GitHub login
+//     console.log('GitHub login');
+//   };
+
+//   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     setEmail(e.target.value);
+//   };
+
+//   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     setPassword(e.target.value);
+//   };
+
+//   return (
+//     <Box maxWidth="md" mx="auto" mt={8} p={4}>
+//       <form onSubmit={handleSubmit}>
+//         <Stack spacing={4}>
+//           <FormControl id="email">
+//             <FormLabel>Email</FormLabel>
+//             <Input
+//               type="email"
+//               placeholder="Enter your email"
+//               value={email}
+//               onChange={handleEmailChange}
+//             />
+//           </FormControl>
+
+//           <FormControl id="password">
+//             <FormLabel>Password</FormLabel>
+//             <Input
+//               type="password"
+//               placeholder="Enter your password"
+//               value={password}
+//               onChange={handlePasswordChange}
+//             />
+//           </FormControl>
+
+//           <Button type="submit" colorScheme="blue">
+//             Log In
+//           </Button>
+
+//           <Center>
+//             <Text fontSize="sm">Or sign in with</Text>
+//           </Center>
+
+//           <Divider />
+
+//           <Center>
+//             <Button
+//               leftIcon={<Image src={GoogleIcon} alt="Google Logo" />}
+//               onClick={handleGoogleLogin}
+//             >
+//               Sign in with Google
+//             </Button>
+//           </Center>
+
+//           <Center>
+//             <Button
+//               leftIcon={<Image src={GithubIcon} alt="GitHub Logo" />}
+//               onClick={handleGitHubLogin}
+//             >
+//               Sign in with GitHub
+//             </Button>
+//           </Center>
+//         </Stack>
+//       </form>
+//     </Box>
+//   );
+// };
+
+// export default LoginForm;
