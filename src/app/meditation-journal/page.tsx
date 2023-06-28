063e1974e65f8a7c9ac4a7f95949a0626e927ee4
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-27 21:38:44 -0400

    Rename file to fix naming bug.

---
+++ src/app/meditation-journal/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import JournalForm from '../components/JournalForm'
+
+function JournalPage() {
+    return (
+        <div>
+            <h1>Meditation Journal Page</h1>
+            <JournalForm />
+        </div>
+    );
+}
+
+export default JournalPage;
