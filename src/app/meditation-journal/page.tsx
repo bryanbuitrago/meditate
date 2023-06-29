4bded33df21133becda6330fade995a1757e689c
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-06-29 18:06:05 -0400

    Add Route Protection.

--- src/app/meditation-journal/page.tsx
+++ src/app/meditation-journal/page.tsx
@@ -1,14 +1,30 @@
 'use client'
 
-import JournalForm from '../components/JournalForm'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import JournalForm from '../components/JournalForm';
 
 function JournalPage() {
-    return (
-        <div>
-            <h1>Meditation Journal Page</h1>
-            <JournalForm />
-        </div>
-    );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === 'loading') {
+    // Optional: Show a loading spinner or skeleton UI
+    return <div>Loading...</div>;
+  }
+
+  if (!session) {
+    // User is not authenticated, redirect to login page
+    router.push('/');
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>Meditation Journal Page</h1>
+      <JournalForm />
+    </div>
+  );
 }
 
 export default JournalPage;
--- src/app/meditation-timer/page.tsx
+++ src/app/meditation-timer/page.tsx
@@ -1,7 +1,23 @@
 'use client'
 
 import Timer from '../components/Timer';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
 function TimerPage() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+  
+    if (status === 'loading') {
+      // Optional: Show a loading spinner or skeleton UI
+      return <div>Loading...</div>;
+    }
+  
+    if (!session) {
+      // User is not authenticated, redirect to login page
+      router.push('/');
+      return null;
+    }
     return (
         <div>
             <h1>Meditation Timer Page</h1>
