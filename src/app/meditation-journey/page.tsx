7bffdaefc99a8124fffa5b381c0bd15940a586a4
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-07-04 00:02:40 -0400

    Add mock data & component card.

--- src/app/meditation-journey/page.tsx
+++ src/app/meditation-journey/page.tsx
@@ -1,9 +1,47 @@
+'use client'
+
+import { Box, Center} from '@chakra-ui/react';
+import JourneyRectangleCard from '../components/JourneyRectCard';
+
 function JourneyPage() {
+
+    const cards = [
+        {
+          date: '2023-06-30',
+          time: '10:30 AM',
+          text: 'Lorem ipsum dolor sit amet.',
+          duration: 3600, // 1 hour
+        },
+        {
+          date: '2023-07-01',
+          time: '2:45 PM',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          duration: 1800, // 30 minutes
+        },
+        {
+          date: '2023-07-02',
+          time: '8:15 AM',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+          duration: 120, // 2 minutes
+        },
+      ];
+
+      
     return (
         <div>
+
             <h1>Meditation Journey Page/History </h1>
+            <Center h="100vh">
+        <Box>
+          <JourneyRectangleCard cards={cards} />
+        </Box>
+      </Center>
+
         </div>
     );
 }
 
 export default JourneyPage;
+
+
+
