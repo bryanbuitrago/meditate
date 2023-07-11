a45b0244299b5f8691569b83edab00da47b17ec9
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-07-10 20:58:06 -0400

    Create logic to format date.

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,22 +1,45 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { format, parse, parseISO } from "date-fns";
+import { start } from "repl";
 
 type SessionFieldTypes = {
     time: number
+    startDateTime: string
     email: string
 };
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const prisma = new PrismaClient();
 
-  const { time, email }: SessionFieldTypes = await req.json();
+  const { time, startDateTime, email }: SessionFieldTypes = await req.json();
+  console.log('startDateTime= ', startDateTime)
+  console.log('startDateTime Typeof= ', typeof(startDateTime))
+  console.log('Timer API Values= ', time, startDateTime, email)
 
-  console.log('Timer API Values= ', time, email)
+  //   Save [Date/Time] As A String Format 
+  const stringFormatDateTime = format(new Date(startDateTime), "yyyy-MM-dd HH:mm:ss");
+  console.log('Formatted start time:', stringFormatDateTime);
+
+  // Save [Date/Time] As Date Time Object Format
+  const dateTimeFormat = parse(stringFormatDateTime, "yyyy-MM-dd HH:mm:ss", new Date());
+  console.log('Type of parsed start time:', typeof dateTimeFormat);
+  console.log('Parsed start time:', dateTimeFormat);
+
+// const dateTimeFormat = new Date(stringFormatDateTime)
+// console.log(typeof(dateTimeFormat))
+// console.log(dateTimeFormat)
+// console.log(parsedStartTimeDate)
+// const parsedDateTime = parseISO(startTimeDate)
+// console.log(parsedDateTime)
+
+
 
   try {
     const sessionEntry = await prisma.session.create({
       data: {
         time,
+        startDateTime: dateTimeFormat,
         user: {
           connect: { email },
         },
