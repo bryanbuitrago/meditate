88c644fe0080d022c98ff50983cd9bf12d6ee16f
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-07-26 17:29:33 -0400

    Remove console.logs.

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt'
+import { hashPassword } from "@/lib/authUtils";
 
 type UserCredentials = {
   username: string;
@@ -14,22 +14,18 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
   const { username, email, password }: UserCredentials = await req.json();
 
-  console.log('credentials=', username, email, password);
-
-  const hashedPassword = await bcrypt.hash(password, 12)
-
-  console.log(hashedPassword)
+  const hashedPassword = await hashPassword(password)
   
   try {
     const user = await prisma.user.create({
       data: {
         username,
         email,
-        password: hashedPassword,
+        password: hashedPassword
       },
     });
-
-    return NextResponse.json({ user });
+    
+    return NextResponse.json(user);
   } catch (error) {
     console.error(error);
     return NextResponse.json({
