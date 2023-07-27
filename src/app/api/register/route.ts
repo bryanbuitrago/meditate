cc21d56796223c78ea912d0520f481c73df7b06e
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-07-26 22:42:40 -0400

    Code refactor & error handling.

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,48 +1,75 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient, User } from "@prisma/client";
 import { hashPassword } from "@/utils/authUtils";
 
 type UserCredentials = {
   username: string;
   email: string;
   password: string;
-};
+}
 
-export async function POST(req: NextRequest) {
-
+export async function POST(req: NextRequest): Promise<NextResponse> {
   const prisma = new PrismaClient();
 
   const { username, email, password }: UserCredentials = await req.json();
 
+  // Validate user data
   if (
-    !email ||
-    !email.includes('@') ||
-    !password ||
-    password.trim().length < 7
-  ) {
+      !email || 
+      !email.includes('@') || 
+      !password ||
+      password.trim().length < 7
+     ) 
+    {
     return NextResponse.json({
       message: 'Invalid input - Password should be at least 7 characters long.',
-    })
+    }, {
+      status: 422
+    });
   }
-  
-  const hashedPassword = await hashPassword(password)
-  
+
+  // Check if user exists in DB
+  const isUserInDB: User | null = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+  // If user exists throw an error
+  if(isUserInDB) {
+    return NextResponse.json({
+      message: 'User already exists!',
+    }, {
+      status: 422
+    });
+  }
+
+  // Hash password 
+  const hashedPassword: string = await hashPassword(password);
+
+  // Attempt to create user in DB 
   try {
-    const user = await prisma.user.create({
+    const user: User = await prisma.user.create({
       data: {
         username,
         email,
         password: hashedPassword
       },
     });
-
-    return NextResponse.json(user);
-  } catch (error) {
+    // User successfully created
+    return NextResponse.json({
+      message: 'User successfully created!'
+    }, {
+      status: 201
+    });
+    
+    // Server Error
+  } catch (error: any) {
     console.error(error);
     return NextResponse.json({
-        message: 'Internal server error'
+      message: 'Internal server error'
     }, {
-        status: 500,
-    })
+      status: 500,
+    });
   }
 }
