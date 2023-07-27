dd5bb51001ac913d74beffab9f722f3fc4147706
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-07-26 21:42:51 -0400

    Remove unused code.

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { format, parse, parseISO } from "date-fns";
-import { start } from "repl";
+import { format, parse} from "date-fns";
+
+
 
 type SessionFieldTypes = {
     time: number
@@ -26,15 +27,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
   console.log('Type of parsed start time:', typeof dateTimeFormat);
   console.log('Parsed start time:', dateTimeFormat);
 
-// const dateTimeFormat = new Date(stringFormatDateTime)
-// console.log(typeof(dateTimeFormat))
-// console.log(dateTimeFormat)
-// console.log(parsedStartTimeDate)
-// const parsedDateTime = parseISO(startTimeDate)
-// console.log(parsedDateTime)
-
-
-
   try {
     const sessionEntry = await prisma.session.create({
       data: {
@@ -51,17 +43,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
       where: { email },
       data: { sessions: { connect: { id: sessionEntry.id } } },
     });
-
-    const user = await prisma.user.findUnique({
-      where: { email },
-      include: { sessions: true }
-    })
-
-
-    console.log(sessionEntry)
-    console.log('=================')
-    console.log(user?.sessions)
-    return NextResponse.json({ sessionEntry, updatedUser, user });
+    return NextResponse.json({ sessionEntry, updatedUser });
   } catch (error) {
     console.error(error);
     return NextResponse.json({
