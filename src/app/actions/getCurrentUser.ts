cdd13c5ad80cd3355f846af30c83e16458bf8963
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-04 14:05:01 -0400

    Create get current user action.

---
+++ src/app/actions/getCurrentUser.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/utils/authOptions";
+import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/react";
+
+export default async function getCurrentUser() {
+    const prisma = new PrismaClient()
+    try {
+        const serverSession = await getServerSession(authOptions)
+
+        if(!serverSession?.user?.email) {
+            return null
+        }
+
+        const currentUser = await prisma.user.findUnique({
+            where: {
+                email: serverSession.user.email as string,
+            }
+        })
+
+        if(!currentUser) {
+            return null
+        }
+
+        return  {
+            ...currentUser,
+            createdAt: currentUser.createdAt.toISOString(),
+            updatedAt: currentUser.updatedAt.toISOString(),
+        }
+    }
+    catch( error: any) {
+        return null   
+    }
+    
+}
