7f03bdc1a9836b072a0dffe412d8995fb84551aa
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-11 16:40:01 -0400

    Fetches the single journal correctly.

--- src/app/journals/[journalID]/page.tsx
+++ src/app/journals/[journalID]/page.tsx
@@ -4,24 +4,20 @@ import { Text } from '@chakra-ui/react';
 import { useRouter } from "next/router";
 
 
-async function SingleJournal({ params: { journalID }}) {
-// async function SingleJournal(props) {
-//     console.log('[SingleJournalPage]: ', props)
-// console.log(await getJournalByID(journalID))
-const journal = await getJournalByID(journalID)
+type JournalType = {
+    journalID: string
+}
 
-console.log('[Journal from DB] :', journal)
 
-    // console.log('Journal ID', typeof(journalID))
+async function SingleJournal({ params } : { params: JournalType }) {
+
+const journal = await getJournalByID(params)
+
     return (
         <div>
             Single Journal Page
             <h1>{journal?.title}</h1>
             <p>{journal?.text}</p>
-            {/* <h1>{journal}</h1> */}
-            {/* <h1>{journal?.title}</h1>
-            <p>{journal?.text}</p>
-            <p>{router.query.journalID}</p> */}
         </div>
     );
 }
