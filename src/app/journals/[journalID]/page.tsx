97213505951f75ed37af8d20e7c3c8fadde7d034
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-16 17:34:46 -0400

    Import UpdateJournal.

--- src/app/journals/[journalID]/page.tsx
+++ src/app/journals/[journalID]/page.tsx
@@ -1,25 +1,46 @@
+// === 2nd Version ===
 
+import SingleJournal from "@/app/(components)/journal/SingleJournal";
 import { getJournalByID } from "@/app/actions/journal/journalActions";
-import { Text } from '@chakra-ui/react';
-import { useRouter } from "next/router";
-
+import UpdateJournal from "@/app/(components)/journal/UpdateJournal";
 
 type JournalType = {
-    journalID: string
+    journalID: string;
 }
 
+async function SingleJournalPage({ params } : {params: JournalType} ) {
 
-async function SingleJournal({ params } : { params: JournalType }) {
-
-const journal = await getJournalByID(params)
-
+    const journal = await getJournalByID(params)
+    const { title, text, id } = journal
     return (
-        <div>
-            Single Journal Page
-            <h1>{journal?.title}</h1>
-            <p>{journal?.text}</p>
-        </div>
-    );
-}
+        // <SingleJournal title={title} text={text}/>
+        <UpdateJournal title={title} text={text} id={id} />
 
-export default SingleJournal;
+    )
+}
+export default SingleJournalPage
+
+
+// === 1st Version ===
+// import { getJournalByID } from "@/app/actions/journal/journalActions";
+
+
+// type JournalType = {
+//     journalID: string
+// }
+
+
+// async function SingleJournal({ params } : { params: JournalType }) {
+
+// const journal = await getJournalByID(params)
+
+//     return (
+//         <div>
+//             Single Journal Page
+//             <h1>{journal?.title}</h1>
+//             <p>{journal?.text}</p>
+//         </div>
+//     );
+// }
+
+// export default SingleJournal;
