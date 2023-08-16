a25a2c43627b8096aadcb8c64c5d27563d4a9c0c
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-15 23:00:59 -0400

    Create update journal entry component.

---
+++ src/app/(components)/journal/UpdateJournal.tsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useState } from 'react';
+import axios from 'axios';
+
+type Journal = {
+    title: string;
+    text: string;
+    id: string
+}
+
+function UpdateJournal({ title, text, id }: Journal) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [currentTitle, setCurrentTitle] = useState(title);
+    const [currentText, setCurrentText] = useState(text);
+
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    }
+
+    const handleSubmitClick = async (e) => {
+        e.preventDefault()
+        setIsEditing(false)
+
+        const updatedJournal = {
+            title: currentTitle,
+            text: currentText
+        }
+        try {
+           await axios.put(`/api/journal/${id}`, updatedJournal)  
+            
+        } catch (error: any) {
+            console.error('Error updating the jorunal: ', error)
+        }
+
+
+        // Here you can handle the submission logic, like sending the updated data to an API.
+    }
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentTitle(e.target.value);
+    }
+
+    const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setCurrentText(e.target.value);
+    }
+
+    return (
+        <div>
+            {isEditing ? (
+                <>
+                    <input type="text" value={currentTitle} onChange={handleTitleChange} />
+                    <textarea value={currentText} onChange={handleTextChange}></textarea>
+                    <button onClick={handleSubmitClick}>Submit</button>
+                </>
+            ) : (
+                <>
+                    <h1>{currentTitle}</h1>
+                    <p>{currentText}</p>
+                    <button onClick={handleEditClick}>Update</button>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default UpdateJournal;
