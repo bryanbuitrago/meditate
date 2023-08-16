e4dd29ec4d4c8d0258d5d342c8fc13d27ba3b090
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-16 17:36:28 -0400

    Import the JournalForm.

---
+++ src/app/journal/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import JournalForm from '../(components)/JournalForm';
+import LoadingSpinner from '../(components)/LoadingSpinner';
+
+function JournalPage() {
+  // const { data: session, status } = useSession();
+  // const router = useRouter();
+
+  // if (status === 'loading') {
+  //   // Optional: Show a loading spinner or skeleton UI
+  //   // return <div>Loading...</div>;
+  //   return <LoadingSpinner />
+  // }
+
+  // if (!session) {
+  //   // User is not authenticated, redirect to login page
+  //   router.push('/');
+  //   return null;
+  // }
+
+  return (
+    <div>
+      <h1>Meditation Journal Page</h1>
+      <JournalForm />
+    </div>
+  );
+}
+
+export default JournalPage;
