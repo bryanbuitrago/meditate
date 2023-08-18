b12cd8fd82ecc3f1beb1d4aa9f6bb9384ffdf70f
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-17 23:17:38 -0400

    Create the DashBoard page and import components.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,19 @@
-function DashBoardPage() {
+import DashBoard from "../(components)/dashboard/DashBoard";
+import UpdateJournal from '../(components)/journal/UpdateJournal';
+import { getLastSubmittedJournal } from "../actions/journal/journalActions";
+
+async function DashBoardPage() {
+    const journal = await getLastSubmittedJournal()
+
+    console.log('[Journal from DB]= ', journal)
+
+    const { title, text, id } = journal
+
     return (
-        <div>
-            <h1>This is the dashboard page.</h1>
-        </div>
+        <>
+            <DashBoard />
+            <UpdateJournal title={title} text={text} id={id} />
+        </>
     );
 }
 
