373b69b172a873ebc465647ef4708a5d374b0eab
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-18 18:12:13 -0400

    Add protected routes middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server"
 export { default } from 'next-auth/middleware'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
-    return NextResponse.redirect(new URL('/login', req.url))
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req })
+
+    if(!token) {
+        return NextResponse.redirect(new URL('/', req.url))
+    }
 }
 
 export const config = {
-    matcher: ['/journal-history']
+    matcher: ['/journal', '/journals/:path*', '/timer', '/meditations/:path*', '/dashboard']
 }
 
