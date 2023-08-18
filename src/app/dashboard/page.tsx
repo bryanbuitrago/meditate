ca5112b9634b8c5c9d3aba8ec9f19deab4716b40
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-17 23:38:58 -0400

    Import getLastMeditation action & component.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,27 @@
+import { get } from "http";
 import DashBoard from "../(components)/dashboard/DashBoard";
 import UpdateJournal from '../(components)/journal/UpdateJournal';
+import SingleMeditation from "../(components)/meditation/SingleMeditation";
 import { getLastSubmittedJournal } from "../actions/journal/journalActions";
+import { getLastMeditationSession } from "../actions/meditation/meditationActions";
 
 async function DashBoardPage() {
     const journal = await getLastSubmittedJournal()
+    const meditation = await getLastMeditationSession()
+
 
     console.log('[Journal from DB]= ', journal)
+    console.log('[Meditation from DB= ', meditation)
+
 
     const { title, text, id } = journal
 
     return (
-        <>
+        <div>
             <DashBoard />
             <UpdateJournal title={title} text={text} id={id} />
-        </>
+            <SingleMeditation meditation={meditation} />
+        </div>
     );
 }
 
