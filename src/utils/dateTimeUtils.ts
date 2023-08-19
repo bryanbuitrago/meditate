f18ef84f382e688b8726277f17b95d06d256d173
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-18 21:46:31 -0400

    Add comments describing functions.

--- src/utils/dateTimeUtils.ts
+++ src/utils/dateTimeUtils.ts
@@ -1,9 +1,11 @@
 import format from "date-fns/format";
 
+// Format to display date & time
 export function _formatDate( date: string ) {
     return format(new Date(), 'yyyy-MM-dd HH:mm:ss')
 } 
 
+// Format to display the length of meditation session
 export function _formatDuration (duration: number): string {
     const hours = Math.floor(duration / 3600);
     const minutes = Math.floor((duration % 3600) / 60);
