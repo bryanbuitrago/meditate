6f3822c0357ae446820f7be94cf0d5852c4ae951
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-19 01:26:57 -0400

    (components) folder rename to components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import styles from './page.module.css'
-import HomePageGreeting from './(components)/HomePageGreeting';
+import HomePageGreeting from './components/HomePageGreeting';
 
 export default function Home() {
   return (
