150c90a8a78775933f6f55dde47553a947f8c220
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-19 02:04:54 -0400

    Remove unused commented lines.

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -43,39 +43,3 @@ function Navbar({ currentUser }) {
 }
 
 export default Navbar;
-
-
-
-// === Version 1 ===
-
-// 'use client'
-
-// import { signOut } from "next-auth/react";
-// import Link from "next/link";
-
-
-
-// // Receive current user as prop
-// function Navbar({ currentUser }) {
-//     return (
-//         <header>
-//             <div>{currentUser?.name}</div>
-
-//             <div>
-//                 <Link href='/journal'>Journal</Link>
-//                 <Link href='/journals'>Journals</Link>
-//                 <Link href='/timer'>Timer</Link>
-//                 <Link href='/meditations'>Meditations</Link>
-//                 <Link href='/dashboard'>Dashboard</Link>
-//                 {
-//                     currentUser ? 
-//                         <button onClick={() => signOut()}>Sign out</button>
-//                     :
-//                         <Link href='/register'>Register</Link>
-//                 }
-//             </div>
-//         </header>
-//     );
-// }
-
-// export default Navbar;
