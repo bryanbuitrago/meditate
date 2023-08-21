83bb0e83a351a0565641dbab9e01997a9401e5b4
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-21 15:58:00 -0400

    Remove unsued import and code cleanup.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,20 @@
-import { get } from "http";
 import DashBoard from "../components/dashboard/DashBoard";
 import SingleJournal from '../components/journal/SingleJournal';
 import SingleMeditation from "../components/meditation/SingleMeditation";
 import { getLastSubmittedJournal } from "../actions/journal/journalActions";
 import { getLastMeditationSession } from "../actions/meditation/meditationActions";
 
-async function DashBoardPage() {
+export default async function DashBoardPage() {
     
     const journal = await getLastSubmittedJournal()
     const meditation = await getLastMeditationSession()
-    const { title, text, id } = journal
+    const { title, text, id, createdAt } = journal
 
     return (
         <div>
             <DashBoard />
-            <SingleJournal title={title} text={text} id={id} />
+            <SingleJournal title={title} text={text} id={id} createdAt={createdAt} />
             <SingleMeditation meditation={meditation} />
         </div>
     );
 }
-
-export default DashBoardPage;
