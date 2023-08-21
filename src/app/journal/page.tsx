a6402c42271a6f512e8ae011d87a45c696d226f7
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-20 23:14:54 -0400

    Remove unsued commented code.

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -6,20 +6,6 @@ import JournalForm from '../components/journal/JournalForm';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 function JournalPage() {
-  // const { data: session, status } = useSession();
-  // const router = useRouter();
-
-  // if (status === 'loading') {
-  //   // Optional: Show a loading spinner or skeleton UI
-  //   // return <div>Loading...</div>;
-  //   return <LoadingSpinner />
-  // }
-
-  // if (!session) {
-  //   // User is not authenticated, redirect to login page
-  //   router.push('/');
-  //   return null;
-  // }
 
   return (
     <div>
