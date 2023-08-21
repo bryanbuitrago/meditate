a036f8f1358282a5dffa8f3bfcae4a7d5ce6918f
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-21 13:55:22 -0400

    Turn into server component & code cleanup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-'use client'
+import HomeComponent from './components/home/HomeComponent';
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import HomePageGreeting from './components/HomePageGreeting';
-
-export default function Home() {
-  return (
-    <div>
-   <h1>Welcome to the Meditation App</h1>
-   <HomePageGreeting />
-   </div>
-  )
+export default function HomePage() {
+    return (
+      <HomeComponent />
+    )
 }
