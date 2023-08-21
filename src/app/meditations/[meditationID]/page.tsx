f89266a455a1f4ab07e6e6d09c7ba79d8a0d6127
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-21 17:44:28 -0400

    Modify to pass multiple attributes to component.

--- src/app/meditations/[meditationID]/page.tsx
+++ src/app/meditations/[meditationID]/page.tsx
@@ -5,15 +5,34 @@ type ParamsMeditationID = {
     meditationID: string
 }
 
+type Meditation = {
+    time: number
+    startDateTime: Date
+    createdAt: Date
+}
+
 
 async function SingleMeditationPage({ params } : { params: ParamsMeditationID }) {
 
-const meditation = await getMeditationByID(params)
+    const meditation: Meditation | null = await getMeditationByID(params)
 
-console.log(meditation)
+    if(!meditation) {
+        return <div>Loading or error message... </div>
+    
+    }
+
+    // Format startDateTime Date object to a string
+    const startDateTimeStringFormat = meditation.startDateTime.toISOString()
+
+    // Format createdAt Date object to a string
+    const createdAtStringFormat = meditation.createdAt.toISOString()
 
     return (
-        <SingleMeditation meditation={meditation} />
+        <SingleMeditation 
+            time={meditation.time}
+            startDateTime={startDateTimeStringFormat}
+            createdAt={createdAtStringFormat}
+        />
     );
 }
 
