af1421a810be987a5576e190d9209556f1b49f37
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-20 23:21:22 -0400

    Remove unused commented code.

--- src/app/journals/[journalID]/page.tsx
+++ src/app/journals/[journalID]/page.tsx
@@ -11,34 +11,8 @@ async function SingleJournalPage({ params } : {params: JournalType} ) {
     const journal = await getJournalByID(params)
     const { title, text, id } = journal
     return (
-        // <SingleJournal title={title} text={text}/>
-        <SingleJournal title={title} text={text} id={id} />
 
+        <SingleJournal title={title} text={text} id={id} />
     )
 }
 export default SingleJournalPage
-
-
-// === 1st Version ===
-// import { getJournalByID } from "@/app/actions/journal/journalActions";
-
-
-// type JournalType = {
-//     journalID: string
-// }
-
-
-// async function SingleJournal({ params } : { params: JournalType }) {
-
-// const journal = await getJournalByID(params)
-
-//     return (
-//         <div>
-//             Single Journal Page
-//             <h1>{journal?.title}</h1>
-//             <p>{journal?.text}</p>
-//         </div>
-//     );
-// }
-
-// export default SingleJournal;
