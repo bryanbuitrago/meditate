611b2500028d1673cdca7834518d0e0f0fd15462
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-22 14:09:12 -0400

    Import components & position them in a row.

--- src/app/components/dashboard/DashBoard.tsx
+++ src/app/components/dashboard/DashBoard.tsx
@@ -1,9 +1,18 @@
-function DashBoard() {
-    return (
-        <div>
-            <h1>Show The Last Journal & Last Meditation Session</h1>
-        </div>
-    );
+'use client'
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import JournalLatest from '../journal/JournalLatest';
+import MeditationLatest from '../meditation/MeditationLatest';
+
+function DashBoard({ journal, meditation }: any) {
+  console.log('[Meditation from DashBoard]= ', meditation);
+  return (
+    <Box p="4" boxShadow="md" borderRadius="md">
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
+        <JournalLatest journal={journal} />
+        <MeditationLatest meditation={meditation} />
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export default DashBoard;
