9775774068fd3e4888b2535ca7e9184159360571
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-22 11:40:29 -0400

    Code cleanup.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,13 @@
 import DashBoard from "../components/dashboard/DashBoard";
-import SingleJournal from '../components/journal/SingleJournal';
-import SingleMeditation from "../components/meditation/SingleMeditation";
-import { getLastSubmittedJournal } from "../actions/journal/journalActions";
-import { getLastMeditationSession } from "../actions/meditation/meditationActions";
+import { getLastSubmittedJournal } from "../actions/journal/journalActions"
+import { getLastMeditationSession } from "../actions/meditation/meditationActions"
 
 export default async function DashBoardPage() {
     
     const journal = await getLastSubmittedJournal()
     const meditation = await getLastMeditationSession()
-    const { title, text, id, createdAt } = journal
 
     return (
-        <div>
-            <DashBoard />
-            <SingleJournal title={title} text={text} id={id} createdAt={createdAt} />
-            <SingleMeditation meditation={meditation} />
-        </div>
+            <DashBoard journal={journal} meditation={meditation} />
     );
 }
